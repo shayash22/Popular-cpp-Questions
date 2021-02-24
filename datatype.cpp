61d364ebc2c626b46e4f7347593815b2c5276311
Shayash Jena <shayash2203@gmail.com>
2021-02-24 23:08:31 +0530

    a

---
+++ datatype.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int a; //declaration
+    a=12; //initailisation
+    cout<<"Size of int: "<<sizeof(a)<<"\n";
+    float b;
+    cout<<"Size of float: "<<sizeof(b)<<"\n";
+    char c;
+    cout<<"Size of char: "<<sizeof(c)<<"\n";
+    bool d;
+    cout<<"Size of bool: "<<sizeof(d)<<"\n";
+    short int si;
+    cout<<"Size of sort int: "<<sizeof(si)<<"\n";
+    long int li;
+    cout<<"Size of long int: "<<sizeof(li)<<"\n";
+    long long int lli;
+    cout<<"Size of long long int: "<<sizeof(lli)<<"\n";
+    
+    return 0;
+}
