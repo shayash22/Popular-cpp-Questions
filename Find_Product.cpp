a941ac6d0227eae811f4f6e40ef1465c292e775e
Shayash Jena <shayash2203@gmail.com>
2021-02-28 22:20:59 +0530

    add

---
+++ Find_Product.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define m1 1000000007
+#define int long long
+#define float double
+#define vi vector<int>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) c.begin(), c.end()
+#define mid(l, r) (l + (r - l) / 2)
+#define loop(i, a, b) for(int i = (a); i<= (b); i++)
+#define loopneg(i, a, b) for(int i = (a); i>= (b); i--)
+#define arll(n) long long ar[n]
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+ 
+int32_t main(){
+ 
+    fast
+    int n;
+    cin>>n;
+    int ar[n];
+    int mul=1;
+    for(int i = 0; i<n; i++){
+        cin>>ar[i];
+        mul=(mul*ar[i])%m1;
+    }
+    cout<<mul%m1<<endl;
+}
