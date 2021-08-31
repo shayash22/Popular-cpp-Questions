82901eedf67c79d3de3c4912243817b7bd4886f6
shayash22 <67068946+shayash22@users.noreply.github.com>
2021-08-31 19:10:27 +0530

    6

---
+++ PrepInsta/100 code/6_greatestOfTheThreeNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n1,n2,n3;
+    cin>>n1>>n2>>n3;
+    if(n1>=n2 && n1>=n3){
+        cout<<n1<<" is greatest"<<endl;
+    }
+    else if(n2>=n1 && n2>=n3){
+        cout<<n2<<" is greatest."<<endl;
+    }
+    else{
+        cout<<n3<<" is greatest"<<endl;
+    }
+    return 0;
+}
